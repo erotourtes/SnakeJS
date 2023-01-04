edc78c47a7ad7e0548a9476bde74568a20a8ead2
sirmax <erotourtes@gmail.com>
2023-01-04 10:29:32 +0200

    pixi.js is installed

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-import Snake from "./src/snake.js";
-import Field from "./src/field.js";
+// import Snake from "./src/snake.js";
+// import Field from "./src/field.js";
+//
+// const snake = new Snake(0, 0);
+// const field = new Field();
+//
+// const keyMap = {
+//   KeyW: "up",
+//   KeyS: "down",
+//   KeyA: "left",
+//   KeyD: "right",
+// };
+//
+// document.onkeydown = (event) => {
+//   snake.changeDirection(keyMap[event.code]);
+// };
+//
+// field.generate();
 
-const snake = new Snake(0, 0);
-const field = new Field();
+import * as PIXI from "pixi.js";
 
-const keyMap = {
-  KeyW: "up",
-  KeyS: "down",
-  KeyA: "left",
-  KeyD: "right",
-};
+const app = new PIXI.Application({width: 256, height: 256});
 
-document.onkeydown = (event) => {
-  snake.changeDirection(keyMap[event.code]);
-};
 
-field.generate();
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+app.renderer.backgroundColor = 0x061639;
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.resizeTo = window;
