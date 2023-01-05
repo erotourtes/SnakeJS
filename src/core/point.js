72611596597ff8b8692105baebb7e5b2fda1e34d
sirmax <erotourtes@gmail.com>
2023-01-05 16:56:46 +0200

    get snake position

--- src/core/point.js
+++ src/core/point.js
@@ -12,6 +12,23 @@ class Point {
   raw() {
     return [this.x, this.y];
   }
+
+  divide(number) {
+    this.x /= number;
+    this.y /= number;
+  }
+
+  static subtract(point1, point2) {
+    return new Point(point1.x - point2.x, point1.y - point2.y);
+  }
+
+  static add(point1, point2) {
+    return new Point(point1.x + point2.x, point1.y + point2.y);
+  }
+
+  static of(point) {
+    return new Point(point.x, point.y);
+  }
 }
 
 export default Point;
--- src/core/uiSnake.js
+++ src/core/uiSnake.js
@@ -10,30 +10,44 @@ class UiSnake extends Snake {
   constructor(tileSize, canvasSize, draw) {
     super(new Point(0, 0), tileSize);
     this.tileSize = tileSize;
-
-    this.handleWorldPosition(canvasSize, tileSize);
-    this.createBodyUI(canvasSize);
+    this.handleSpawnPosition(canvasSize, tileSize);
+    this.createBodyUI();
 
     draw(this._bodyUI[0]);
   }
 
-  createBodyUI(canvasSize) {
+  createBodyUI() {
     const circle = new Graphics();
-    circle.beginFill(0x0000FF).drawCircle(canvasSize.x / 2, canvasSize.y / 2, this.tileSize / 2).endFill();
+    circle.beginFill(0x0000FF).drawCircle(...this.startPosition.raw(), this.tileSize / 2).endFill();
     this._bodyUI.push(circle);
   }
 
   updatePosition() {
       this.move();
-      const [x, y] = this._body[0].raw();
+      const [x, y] = this.head.raw();
       this._bodyUI[0].position.set(x, y);
+
+    console.log(this.worldPosition);
   }
 
-  handleWorldPosition(position, tileSize) {
-    if ((position.x / tileSize) % 2 === 0)
-      position.move(new Vector(-tileSize, 0));
-    if((position.y / tileSize) % 2 === 0)
-      position.move(new Vector(0, -tileSize));
+  handleSpawnPosition(position, tileSize) {
+    this.startPosition = Point.of(position);
+
+    if ((this.startPosition.x / tileSize) % 2 === 0)
+      this.startPosition.move(new Vector(-tileSize, 0));
+    if((this.startPosition.y / tileSize) % 2 === 0)
+      this.startPosition.move(new Vector(0, -tileSize));
+
+    this.startPosition.divide(2);
+  }
+
+  get worldPosition() {
+    const circleCenter = Point.add(this.startPosition, this.head);
+    return Point.subtract(circleCenter, new Point(this.tileSize / 2, this.tileSize / 2));
+  }
+
+  get head() {
+    return this._body[0];
   }
 }
 
