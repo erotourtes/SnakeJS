981834aac6b9252e87348a6a45528ae477a93725
sirmax <erotourtes@gmail.com>
2023-01-07 18:40:17 +0200

    effects

--- src/core/effects.js
+++ src/core/effects.js
@@ -3,10 +3,59 @@ import { filters, Ticker } from "pixi.js";
 class Effects {
   constructor(container) {
     this.container = container;
+    this.methods = {
+      "colorize": this.colorize,
+      "blackAndWhite": this.blackAndWhite,
+      "lsd": this.lsd,
+      "night": this.night,
+      "negative": this.negative,
+    }
   }
 
   createEffect(ms) {
-    this.colorize(ms);
+    const keys = Object.keys(this.methods);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+
+    const filter = this.methods[randomKey];
+
+    filter.call(this, ms);
+  }
+
+  negative(ms) {
+    const filter = new filters.ColorMatrixFilter();
+    filter.negative();
+    this.container.filters = [filter];
+    this.clear(ms);
+  }
+
+  night(ms) {
+    const filter = new filters.ColorMatrixFilter();
+    filter.night();
+    this.container.filters = [filter];
+
+    this.clear(ms);
+  }
+
+  lsd(ms) {
+    const filter = new filters.ColorMatrixFilter();
+    filter.lsd();
+    this.container.filters = [filter];
+
+    this.clear(ms);
+  }
+
+  blackAndWhite(ms) {
+    const filter = new filters.ColorMatrixFilter();
+    filter.blackAndWhite();
+    this.container.filters = [filter];
+
+    this.clear(ms);
+  }
+
+  clear(ms) {
+    setTimeout(() => {
+      this.container.filters = [];
+    }, ms);
   }
 
   colorize(ms) {
